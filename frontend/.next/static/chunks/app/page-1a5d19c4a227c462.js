fdbe131ed39621cfdf92897bc558feaecb29575e
sammytamimi <89275325+sammytamimi@users.noreply.github.com>
2025-06-05 13:04:06 +0100

    Update project structure

---
+++ frontend/.next/static/chunks/app/page-1a5d19c4a227c462.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[974],{4397:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,4893,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[893,789,20,358],()=>s(4397)),_N_E=e.O()}]);
